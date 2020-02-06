c740a74e25039baa8cc7a099aa3587ac63c383c8
Aaron Leu <aaronaleu@gmail.com>
2020-02-06 11:39:19 -0500

    Fix: Appended to index.js

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
 const sum = require('./sum');
+const subtract = require('./subtract');
+const multiply = require('./multiply');
+const divide = require('./divide');
+const square = require('./square');
+const squareroot = require('./squareroot');
+
 a = 1;
 b = 2;
+a2 = 8;
+b2 = 64;
 
 c = sum(a, b);
+d = subtract(a, b);
+e = multiply(b, a2);
+f = divide(a2, b2);
+g = square(a2);
+h = squareroot(b2);
 
-console.log("The answer is: " + c);
+
+console.log(a + ' + ' + b + ' = ' + c);
+console.log(b + ' - ' + a + ' = ' + d);
+console.log(b + ' X ' + a2 + ' = ' + e);
+console.log(b2 + ' / ' + a2 + ' = ' + f);
+console.log(a2 + ' squared is ' + g);
+console.log('The square root of ' + b2 + ' is ' + h);
